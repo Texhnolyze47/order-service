e3b40f50206356aaaf85e48ae520d31395c6328b
Texhnolyze47 <ichigobankai136@gmail.com>
2023-02-18 16:49:05 -0600

    estructura inicial de order-service

---
+++ src/main/java/com/texhnolyze/orderservicemicroservice/OrderServiceMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.texhnolyze.orderservicemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/texhnolyze/orderservicemicroservice/OrderServiceMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.texhnolyze.orderservicemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
