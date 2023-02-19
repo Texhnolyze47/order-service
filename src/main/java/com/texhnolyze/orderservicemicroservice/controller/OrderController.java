28c00fb65d7f87d9376cb5d050a23a675d41edfb
Texhnolyze47 <ichigobankai136@gmail.com>
2023-02-18 20:09:05 -0600

    add: metodo post

--- src/main/java/com/texhnolyze/orderservicemicroservice/controller/OrderController.java
+++ src/main/java/com/texhnolyze/orderservicemicroservice/controller/OrderController.java
@@ -1,15 +1,21 @@
 package com.texhnolyze.orderservicemicroservice.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.texhnolyze.orderservicemicroservice.dto.OrderRequest;
+import com.texhnolyze.orderservicemicroservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
+@RequiredArgsConstructor
 public class OrderController {
 
+    private final OrderService orderService;
     @PostMapping
-    public String placeOrder(){
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest){
+        orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
     }
 }
